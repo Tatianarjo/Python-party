0b9453be4574d6a4abafa6b7878982d389b056b1
Tatiana Johnson <Tatianarjo@gmail.com>
2021-10-21 20:16:33 -0500

    Chapter 5 complete

--- employees/__init__.py
+++ employees/__init__.py
@@ -1,2 +1,3 @@
-from .request import get_all_employees, get_single_employee, create_employee
+from .request import get_all_employees, get_single_employee, create_employee, delete_employee
+
 
--- request_handler.py
+++ request_handler.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal
 import json
 from locations import get_all_locations, get_single_location, create_location, delete_location
-from employees import get_all_employees, get_single_employee, create_employee
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
 
 # Here's a class. It inherits from another class.
@@ -141,6 +141,9 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         if resource == "customers":
             delete_customer(id)
+        
+        if resource == "employees":
+            delete_employee(id)
 
     # Encode the new animal and send in response
         self.wfile.write("".encode())
