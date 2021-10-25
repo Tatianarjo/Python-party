cb9b52c6272cf0e737c386f44a88effa27e4437a
Tatiana Johnson <Tatianarjo@gmail.com>
2021-10-25 16:21:38 -0500

    Chapter 7 Classes and Objects

---
+++ models/location.py
@@ -0,0 +1,9 @@
+class Location():
+
+    def __init__(self, id, name, address, employeeId, animalId):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.employeeId = employeeId
+        self.animalId = animalId
+
