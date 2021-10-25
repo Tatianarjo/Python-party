f4313f972b49bd4818db24ef2bf4c87b9d8a0c0c
Tatiana Johnson <Tatianarjo@gmail.com>
2021-10-25 15:27:48 -0500

    Chapter 6 PUT Request Location

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 import json
-from locations import get_all_locations, get_single_location, create_location, delete_location
+from locations import get_all_locations, get_single_location, create_location, delete_location, update_location
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
 
@@ -166,7 +166,11 @@ class HandleRequests(BaseHTTPRequestHandler):
         if resource == "customers":
             update_customer(id, post_body)
 
+        if resource == "locations":
+            update_location(id, post_body)
+
     # Encode the new animal and send in response
+        self.wfile.write("".encode())
         self.wfile.write("".encode())
         self.wfile.write("".encode())
 
