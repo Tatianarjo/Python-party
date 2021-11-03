a910ff222b9370d72c907fba0348d506b5d984d1
Tatiana Johnson <Tatianarjo@gmail.com>
2021-11-02 22:21:14 -0500

    Chapter 10 Get Employees by Id

--- employees/request.py
+++ employees/request.py
@@ -1,5 +1,6 @@
 import sqlite3
 import json
+import employees
 
 from models import Employee, employee
 
@@ -125,3 +126,27 @@ def delete_employee(id):
 
           if employee_index >= 0:
             EMPLOYEES.pop(employee_index)
+
+def get_employees_by_locationId(location_id):
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+            a.id,
+            a.name,
+            a.location_id
+        from Employee a
+        WHERE a.location_id = ?
+        """, ( location_id, ))
+
+        employees = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            employee = Employee(row['id'], row['name'], row['location_id'])
+            employees.append(employee.__dict__)
+
+    return json.dumps(employees)
--- request_handler.py
+++ request_handler.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_animals_by_locationId
 import json
 from locations import get_all_locations, get_single_location, create_location, delete_location, update_location
-from employees import get_all_employees, get_single_employee, create_employee, delete_employee
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee, get_employees_by_locationId
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer, get_all_customers, get_customers_by_email
 
 
@@ -88,6 +88,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         # items in it, which means the request was for
         # `/resource?parameter=value`
         #http://localhost:8088/animals?location_id=1
+        #http://localhost:8088/employees?location_id=1
         elif len(parsed) == 3:
             ( resource, key, value ) = parsed
 
@@ -99,6 +100,9 @@ class HandleRequests(BaseHTTPRequestHandler):
 
             elif key == "location_id" and resource == "animals":
                 response = get_animals_by_locationId(value)
+
+            elif key == "location_id" and resource == "employees":
+                response = get_employees_by_locationId(value)
         self.wfile.write(response.encode())
 
     # Here's a method on the class that overrides the parent's method.
